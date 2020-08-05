9a5417e3bd8ac7c5d498cd87febb6643162f992e
Rahul Gautam <rahul.gautam21@gmail.com>
2020-08-06 01:45:25 +0530

    Changes in Response

--- api/category/controllers/category.js
+++ api/category/controllers/category.js
@@ -1,8 +1,20 @@
 'use strict';
+const { sanitizeEntity } = require('strapi-utils');
 
 /**
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+    async find(ctx) {
+        let entities;
+        if (ctx.query._q) {
+          entities = await strapi.services.category.search(ctx.query);
+        } else {
+          entities = await strapi.services.category.find(ctx.query,['topics','topics.subTopics']);
+        }
+    
+        return entities.map(entity => sanitizeEntity(entity, { model: strapi.models.category }));
+      }
+};
