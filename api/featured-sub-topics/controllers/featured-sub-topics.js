5ece5d6789430308c33c9fe274e2b3eeb92b5ebc
Rahul Gautam <rahul.gautam21@gmail.com>
2020-08-11 01:54:08 +0530

    Fetch Topic also for featured SubTopics

--- api/featured-sub-topics/controllers/featured-sub-topics.js
+++ api/featured-sub-topics/controllers/featured-sub-topics.js
@@ -1,8 +1,16 @@
 'use strict';
+const { sanitizeEntity } = require('strapi-utils');
 
 /**
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+    async findOne(ctx) {
+        const { id } = ctx.params;
+
+        const entity = await strapi.services['featured-sub-topics'].findOne({ id },['subTopics','subTopics.topic','subTopics.topic.categories']);
+        return sanitizeEntity(entity, { model: strapi.models['featured-sub-topics'] });
+    },
+};
