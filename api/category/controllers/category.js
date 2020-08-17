75a0b6a7ae8e1d22248b3d1f0ac059eedbf67836
Rahul Gautam <rahul.gautam21@gmail.com>
2020-08-17 17:29:00 +0530

    Added Image to Category

--- api/category/controllers/category.js
+++ api/category/controllers/category.js
@@ -12,7 +12,7 @@ module.exports = {
         if (ctx.query._q) {
           entities = await strapi.services.category.search(ctx.query);
         } else {
-          entities = await strapi.services.category.find(ctx.query,['topics','topics.subTopics']);
+          entities = await strapi.services.category.find(ctx.query,['image','topics','topics.subTopics']);
         }
     
         return entities.map(entity => sanitizeEntity(entity, { model: strapi.models.category }));
