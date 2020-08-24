3217c49ed8bf36a6b7978af844a1dad0b9409696
Rahul Gautam <rahul.gautam21@gmail.com>
2020-08-24 17:41:40 +0530

    Changing storage link to hdrive

--- config/plugins.js
+++ config/plugins.js
@@ -6,13 +6,13 @@ module.exports = ({ env }) => ({
   
       providerOptions: {
   
-        account: env('STORAGE_ACCOUNT','agastya'),
+        account: env('STORAGE_ACCOUNT','hdrive68138919024'),
   
-        accountKey: env('STORAGE_ACCOUNT_KEY','VGM9rfCXtmESSCQUerjcieH5L5prQEoDzOYd9w6LlsPJdUoEWJWOCbw4vepAKjcKL43sXlbOB2Wc9ti02622Xg=='),
+        accountKey: env('STORAGE_ACCOUNT_KEY','35Z669bQLdih+niQRutUHPynwWD4UBjhHMddPTS1tNH0it+jNE0RTfNqZQNeF12cya6dppEELbUkSryVffY2vA=='),
   
         serviceBaseURL: env('STORAGE_URL'),
   
-        containerName: env('STORAGE_CONTAINER_NAME','agastya'),
+        containerName: env('STORAGE_CONTAINER_NAME','agastya-elearning'),
   
         defaultPath: 'assets',
   
